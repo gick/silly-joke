00d7962ae64e17d7475a8a2e2779dc006972e164
gick <gicquel.pierreyves@gmail.com>
2018-07-31 00:55:32 +0200

    deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      key : '/home/gick/.ssh/gick.key.pub'
+      host : '51.15.135.233',
+      ref  : 'origin/master',
+      repo : 'git@github.com:gick/silly-joke.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'root',
+      key : '/home/gick/.ssh/gick.key.pub'
+      host : '51.15.135.233',
+      ref  : 'origin/master',
+      repo : 'git@github.com:gick/silly-joke.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
