5b8b6b2bbc4d1f9fe1933b57439be9625bdce3d8
gick <gicquel.pierreyves@gmail.com>
2018-07-31 01:44:56 +0200

    ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:gick/silly-joke',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env productio'
     },
     dev : {
       user : 'root',
